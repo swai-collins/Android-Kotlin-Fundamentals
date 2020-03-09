d3f7a617fddc0d4bc2f465d31f1dcb2915a288bc
Swai Collins <swailerc23@gmail.com>
2020-03-09 16:16:38 +0300

    Fix: Changing the function call to generate random numbers

---
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.android.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
+
+        val rollButton: Button = findViewById(R.id.rollBtn)
+        rollButton.setOnClickListener { rollDice() }
+
+        /** val countButton: Button = findViewById(R.id.countBtn)
+        countButton.setOnClickListener{ countUp() } **/
+
+
+    }
+
+    private fun rollDice() {
+        val randomInt= (1..6).random()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        /** val diceImage: ImageView = findViewById(R.id.dice_image) **/
+        diceImage.setImageResource(drawableResource)
+    }
+
+}
+
+
